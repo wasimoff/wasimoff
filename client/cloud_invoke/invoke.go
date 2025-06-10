900e9aae4dab112b459ab8a1e5d8298b82a1c753
semjonov <anton.semjonov@uni-hamburg.de>
2025-06-10 13:29:35 +0200

    client: add example to invoke google cloud run function runner

---
+++ client/cloud_invoke/invoke.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"os"
+
+	"google.golang.org/api/idtoken"
+	"google.golang.org/protobuf/encoding/protojson"
+	"google.golang.org/protobuf/proto"
+	wasimoffv1 "wasi.team/proto/v1"
+)
+
+var (
+	credentials = flag.String("credentials", "account.json", "path to service account json")
+	function    = flag.String("function", "https://wasimoff-runner-308704998937.europe-west10.run.app", "cloud run function url to invoke")
+	readstdin   = flag.Bool("stdin", false, "read from stdin and send with task")
+	verbose     = flag.Bool("v", false, "be more verbose and print messages")
+)
+
+func main() {
+
+	// parse commandline argument, expect exec args in positional
+	flag.Parse()
+	args := flag.Args()
+	if len(args) < 1 {
+		log.Fatalln("args: must give at least a binary name")
+	}
+
+	// create an idtoken client
+	client, err := idtoken.NewClient(context.Background(), *function, idtoken.WithCredentialsFile(*credentials))
+	if err != nil {
+		log.Fatalf("Failed to create idtoken client: %s", err)
+	}
+
+	// create the request
+	request := &wasimoffv1.Task_Wasip1_Request{
+		Info: &wasimoffv1.Task_Metadata{
+			Id: proto.String("gcloud-invoke"),
+		},
+		Params: &wasimoffv1.Task_Wasip1_Params{
+			Binary: &wasimoffv1.File{Ref: proto.String(args[0])},
+			Args:   args,
+		},
+	}
+	// optionally read stdin
+	if *readstdin {
+		stdin, err := io.ReadAll(os.Stdin)
+		if err != nil {
+			log.Fatalln("failed reading stdin:", err)
+		}
+		request.Params.Stdin = stdin
+	}
+
+	// serialize the request json
+	json, err := protojson.Marshal(request)
+	if err != nil {
+		log.Fatalf("Failed to serialize Protobuf request: %s", err)
+	}
+	if *verbose {
+		fmt.Printf("\033[36;1mRequest --> \033[0;36m%s\033[0m\n", string(json))
+	}
+
+	// do the request
+	resp, err := client.Post(*function, "application/json", bytes.NewBuffer(json))
+	if err != nil {
+		log.Fatalf("Failed request: %s", err)
+	}
+	if resp.StatusCode != 200 {
+		log.Fatalf("Request failed: %s", resp.Status)
+	}
+	body, bodyerr := io.ReadAll(resp.Body)
+	if bodyerr != nil {
+		log.Fatalf("Failed to read response body: %v", err)
+	}
+	if *verbose {
+		fmt.Printf("\033[33;1m%s --> \033[0;33m%s\033[0m\n\n", resp.Status, string(body))
+	}
+
+	// decode the response
+	response := &wasimoffv1.Task_Wasip1_Response{}
+	if err := protojson.Unmarshal(body, response); err != nil {
+		log.Fatalf("Failed to decode body as Task_Wasip1_Response: %s", err)
+	} else {
+		ok := response.GetOk()
+		if len(ok.GetStderr()) != 0 {
+			fmt.Fprintf(os.Stderr, "\033[31m%s\033[0m\n", string(ok.GetStderr()))
+		}
+		fmt.Fprint(os.Stdout, string(ok.GetStdout()))
+		os.Exit(int(ok.GetStatus()))
+	}
+
+}
--- client/go.mod
+++ client/go.mod
@@ -1,10 +1,38 @@
 module wasi.team/client
 
-go 1.23.3
+go 1.24.0
 
 require (
+	connectrpc.com/connect v1.18.1
 	github.com/gabriel-vasile/mimetype v1.4.6
-	google.golang.org/protobuf v1.35.2
+	github.com/paulbellamy/ratecounter v0.2.0
+	google.golang.org/api v0.236.0
+	google.golang.org/protobuf v1.36.6
+	wasi.team/broker v0.0.0-20250606130420-88d76e5d6929
+	wasi.team/proto v0.0.0-20250606130420-88d76e5d6929
 )
 
-require golang.org/x/net v0.30.0 // indirect
+require (
+	cloud.google.com/go/auth v0.16.1 // indirect
+	cloud.google.com/go/auth/oauth2adapt v0.2.8 // indirect
+	cloud.google.com/go/compute/metadata v0.7.0 // indirect
+	github.com/coder/websocket v1.8.12 // indirect
+	github.com/felixge/httpsnoop v1.0.4 // indirect
+	github.com/go-logr/logr v1.4.2 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/google/s2a-go v0.1.9 // indirect
+	github.com/googleapis/enterprise-certificate-proxy v0.3.6 // indirect
+	github.com/googleapis/gax-go/v2 v2.14.2 // indirect
+	go.opentelemetry.io/auto/sdk v1.1.0 // indirect
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.60.0 // indirect
+	go.opentelemetry.io/otel v1.35.0 // indirect
+	go.opentelemetry.io/otel/metric v1.35.0 // indirect
+	go.opentelemetry.io/otel/trace v1.35.0 // indirect
+	golang.org/x/crypto v0.38.0 // indirect
+	golang.org/x/net v0.40.0 // indirect
+	golang.org/x/oauth2 v0.30.0 // indirect
+	golang.org/x/sys v0.33.0 // indirect
+	golang.org/x/text v0.25.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20250528174236-200df99c418a // indirect
+	google.golang.org/grpc v1.72.2 // indirect
+)
