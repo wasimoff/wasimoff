c18a0f33f51210a862c4d2355b8510fc8751576e
Anton Semjonov <anton@semjonov.de>
2025-10-02 18:20:52 +0200

    add a protodelim trace output encoder

--- client/cmd/tracebench/log_jsonl.go
+++ client/cmd/tracebench/log_jsonl.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"log"
 	"os"
 	"sync"
@@ -10,18 +9,14 @@ import (
 	"google.golang.org/protobuf/proto"
 )
 
+// Write traces in JSONL format (one trace per line).
 type JsonLineEncoder struct {
 	mutex    sync.Mutex
 	file     *os.File
-	encoder  *json.Encoder
 	protoenc *protojson.MarshalOptions
 }
 
-func OpenOutputLog(filename string) *JsonLineEncoder {
-
-	if filename == "" {
-		return nil
-	}
+func NewJsonLineEncoder(filename string) *JsonLineEncoder {
 
 	file, err := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
 	if err != nil {
@@ -29,9 +24,8 @@ func OpenOutputLog(filename string) *JsonLineEncoder {
 	}
 
 	return &JsonLineEncoder{
-		mutex:   sync.Mutex{},
-		file:    file,
-		encoder: json.NewEncoder(file),
+		mutex: sync.Mutex{},
+		file:  file,
 		protoenc: &protojson.MarshalOptions{
 			Multiline:      false,
 			UseEnumNumbers: false,
@@ -40,26 +34,20 @@ func OpenOutputLog(filename string) *JsonLineEncoder {
 
 }
 
-func (jl *JsonLineEncoder) EncodeJson(v any) error {
-	jl.mutex.Lock()
-	defer jl.mutex.Unlock()
-	return jl.encoder.Encode(v)
-}
-
-func (jl *JsonLineEncoder) EncodeProto(msg proto.Message) error {
-	buf, err := jl.protoenc.Marshal(msg)
+func (enc *JsonLineEncoder) Write(msg proto.Message) error {
+	buf, err := enc.protoenc.Marshal(msg)
 	if err != nil {
 		return err
 	}
 	buf = append(buf, '\n')
-	jl.mutex.Lock()
-	defer jl.mutex.Unlock()
-	_, err = jl.file.Write(buf)
+	enc.mutex.Lock()
+	defer enc.mutex.Unlock()
+	_, err = enc.file.Write(buf)
 	return err
 }
 
-func (jl *JsonLineEncoder) Close() error {
-	jl.mutex.Lock()
-	defer jl.mutex.Unlock()
-	return jl.file.Close()
+func (enc *JsonLineEncoder) Close() error {
+	enc.mutex.Lock()
+	defer enc.mutex.Unlock()
+	return enc.file.Close()
 }
